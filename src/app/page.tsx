bd0d58ced8c9962d2797a829c2d00b8fe95ac4e3
simurgh420 <52625819+simurgh420@users.noreply.github.com>
2025-10-10 12:07:37 +0330

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,8 @@
 
 import Link from 'next/link';
 import Card from '@/components/styled-components/Card';
-import { useEffect, useState } from 'react';
-import Loader from '@/components/styled-components/Loader';
 import MainButton from '@/components/styled-components/Button';
 export default function Home() {
-  const [showLoader, setShowLoader] = useState(true);
-  useEffect(() => {
-    const hasSeenLoader = localStorage.getItem('seenLoader');
-
-    if (hasSeenLoader) {
-      setShowLoader(false);
-    } else {
-      const timer = setTimeout(() => {
-        setShowLoader(false);
-        localStorage.setItem('seenLoader', 'true');
-      }, 2500); // مثلا ۲.۵ ثانیه
-      return () => clearTimeout(timer);
-    }
-  }, []);
-  if (showLoader) {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-black">
-        <Loader />
-      </div>
-    );
-  }
   return (
     <div className="relative min-h-screen bg-neutral-950 text-white">
       <main className="relative z-10 flex flex-col items-center justify-center h-screen text-center px-6">
